2c4458aa61e16aca7e450748e95a02901a98a526
mikloska <mikloska973@gmail.com>
2021-07-04 10:49:28 -0400

    Added Navbar and began working on mobile breakpoints.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 const App = () => {
     return (
         <>
-          <h1>Welcome to Sikra</h1>
+          <Navbar></Navbar>
         
         </>
 )}
