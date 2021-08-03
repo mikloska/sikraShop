6d42f1de15276f915e12a5ec45018d46298e2646
mikloska <mikloska973@gmail.com>
2021-08-03 17:34:58 -0400

    Moved get all products and get product by id routes into separate controller file

---
+++ server/controllers/productController.js
@@ -0,0 +1,41 @@
+import Product from '../models/productModel.js'
+
+// Desc: Fetch all products
+// Route: Get api/products
+// Access: public
+
+const getProducts = async(req,res, next) => {
+  try{
+    res.locals.products=await Product.find({})
+    // console.log('In get all products controller. Res.locals is: ', res.locals)
+    // if(products) return res.json(products)
+    // throw new Error('Error getting products dude')
+    return next()
+  } 
+  catch(error){
+    console.error(`Error: ${error.message}`.red.underline.bold)
+    return next(`Error getting products: ${error.message}`)
+  }
+}
+
+// Desc: Fetch single product
+// Route: Get api/products/:id
+// Access: public
+
+const getProductById = async(req,res, next) => {
+  // console.log('In get product by id controller. Req.params is: ', req.params)
+  try{
+    res.locals.product=await Product.findById(req.params.id)
+    // console.log('In get product by id controller. Res.locals is: ', res.locals)
+    // if(product) return res.json(product)
+    return next()
+  }catch(error){
+    // return res.status(500).json(error.message);
+    return next(new Error(`Product '${req.params.id}' not found`))
+    // return next(error)
+    // console.error(`Error: ${error.message}`.red.underline.bold)
+    // process.exit(1)
+  }
+}
+
+export {getProducts,getProductById}
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,40 +1,43 @@
 import express from 'express'
 import products from '../data/products.js'
+import {getProducts, getProductById} from '../controllers/productController.js'
+
 const router=express.Router()
-import Product from '../models/productModel.js'
 
-// Desc: Fetch all products
-// Route: Get api/products
-// Access: public
-
-router.get('/', async (req, res, next) => {
-  try{
-    const products=await Product.find({})
-    if(products) return res.json(products)
-    // throw new Error('Error getting products dude')
-    return next()
-  } 
-  catch(error){
-    console.error(`Error: ${error.message}`.red.underline.bold)
-    return next(`Error getting products: ${error.message}`)
-  }
+router.get('/', 
+  getProducts,
+  (req, res) => {
+    // console.log('In get all products route. Res.locals is: ', res.locals)
+    return res.status(200).json(res.locals.products)
+  // try{
+  //   const products=await Product.find({})
+  //   if(products) return res.json(products)
+  //   // throw new Error('Error getting products dude')
+  //   return next()
+  // } 
+  // catch(error){
+  //   console.error(`Error: ${error.message}`.red.underline.bold)
+  //   return next(`Error getting products: ${error.message}`)
+  // }
 });
 
-// Desc: Fetch single product
-// Route: Get api/products/:id
-// Access: public
-router.get('/:id', async (req, res, next) => {
-  try{
-    const product=await Product.findById(req.params.id)
-    if(product) return res.json(product)
-    return next()
-  }catch(error){
-    // return res.status(500).json(error.message);
-    return next(new Error(`Product '${req.params.id}' not found`))
-    // return next(error)
-    // console.error(`Error: ${error.message}`.red.underline.bold)
-    // process.exit(1)
-  }
+
+router.get('/:id',
+  getProductById,
+  (req, res) => {
+    // console.log('In get product by id router. Res.locals is: ', res.locals)
+    return res.status(200).json(res.locals.product)
+  // try{
+  //   const product=await Product.findById(req.params.id)
+  //   if(product) return res.json(product)
+  //   return next()
+  // }catch(error){
+  //   // return res.status(500).json(error.message);
+  //   return next(new Error(`Product '${req.params.id}' not found`))
+  //   // return next(error)
+  //   // console.error(`Error: ${error.message}`.red.underline.bold)
+  //   // process.exit(1)
+  // }
   // try{
   //   const product=await Product.findById(req.params.id)
   //   if(product){
