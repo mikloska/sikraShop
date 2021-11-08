e31bef4e3891b35f6be51319a566442684be850e
mikloska <mikloska973@gmail.com>
2021-11-08 10:55:22 -0500

    Fixed home screen screen resize parallax issue

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -55,54 +55,59 @@ const HomeScreen = ({match}) => {
   return (
     <div >
       <Parallax
-        
-        blur={{ min: 15, max: -15 }}
+        blur={{ min: 15, max: -15 }} 
         bgImage={'https://sikra.s3.us-east-2.amazonaws.com/necklaces/silver-bobcat-pendant-outdoor.jpg'}
-        
-        bgImageAlt="bobcat"
-        strength={-200}
+        bgImageAlt="bobcat" 
+        strength={830}
+        renderLayer={percentage => (
+          <div style={{                    left: '50%',
+                    top: '50%',width: percentage * 500, height: percentage * 830}} />
+        )}
       >
-        
-        <div style={{width:'100vw', height: '750px'}} />
       </Parallax>
       <Parallax
-        
         blur={{ min: -15, max: 15 }}
         bgImage={'https://sikra.s3.us-east-2.amazonaws.com/necklaces/ammonite.jpg'}
-        bgImageAlt="ammonite"
-        strength={-200}
-      >
-        
-        <div style={{width:'100vw', height: '750px'}} />
+        bgImageAlt="ammonite" 
+        strength={830}
+        renderLayer={percentage => (
+          <div style={{                    left: '50%',
+                    top: '50%',width: percentage * 830, height: percentage * 830}} />
+        )}
+        >
       </Parallax>
       <Parallax
-        
-        blur={{ min: 15, max: -15 }}
+        blur={{ min: 15, max: -15 }} 
         bgImage={'https://sikra.s3.us-east-2.amazonaws.com/necklaces/owl-group.jpg'}
-        bgImageAlt="owls"
-        strength={-200}
+        bgImageAlt="owls" 
+        strength={830}
+        renderLayer={percentage => (
+          <div style={{                    left: '50%',
+                    top: '50%',width: percentage * 830, height: percentage * 830}} />
+        )}
       >
-      <div style={{width:'100%vw', height: '750px'}} />
       </Parallax>
       <Parallax
-        
-        blur={{ min: 15, max: -15 }}
+        blur={{ min: 15, max: -15 }} 
         bgImage={'https://sikra.s3.us-east-2.amazonaws.com/rings/Dragon-ring-moss.jpg'}
-        bgImageAlt="dragon"
-        strength={-200}
+        bgImageAlt="dragon" 
+        strength={830}
+        renderLayer={percentage => (
+          <div style={{                    left: '50%',
+                    top: '50%',width: percentage * 830, height: percentage * 830}} />
+        )}
       >
-        
-        <div style={{width:'100vw', height: '750px'}} />
       </Parallax>
       <Parallax
-        
         blur={{ min: 10, max: -15 }}
-        bgImage={'https://sikra.s3.us-east-2.amazonaws.com/rings/deep-forest-ring-model.jpg'}
-        bgImageAlt="the dog"
-        strength={-200}
+        bgImage={'https://sikra.s3.us-east-2.amazonaws.com/rings/Fossil-Ring-Gold-Amber-Enamel-Outside-Side.jpg'}
+        bgImageAlt="fossil ring"
+        strength={830}
+        renderLayer={percentage => (
+          <div style={{                    left: '50%',
+                    top: '50%',width: percentage * 830, height: percentage * 830}} />
+        )}
       >
-        
-        <div style={{width:'100vw', height: '750px'}} />
       </Parallax>
       {/* <h1>Latest Products</h1>
       {loading ? <Loader/> : error ? <Message severity='error'>{error}</Message> : 
