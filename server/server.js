f42c8d24fb3d3415a0dfc5f6e4fbb1f48f7f8513
mikloska <mikloska973@gmail.com>
2021-11-15 13:00:52 -0500

    Fixed server file typo. Path.join pointed to build in client folder but it isn't in client.

--- server/server.js
+++ server/server.js
@@ -70,7 +70,7 @@ const __dirname = path.resolve()
 // app.use('/build', express.static('../build'));
 
 // app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
-const root = path.join(__dirname, 'client', 'build')
+const root = path.join(__dirname, 'build')
 if (process.env.NODE_ENV === 'production') {
   // console.log('In production')
   app.use(express.static(root))
