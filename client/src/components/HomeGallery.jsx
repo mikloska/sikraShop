b65667dd0299a052d7a80a571aa4c07edf75b41e
mikloska <mikloska973@gmail.com>
2021-11-16 13:46:17 -0500

    stopped first image blur on home screen gallery

--- client/src/components/HomeGallery.jsx
+++ client/src/components/HomeGallery.jsx
@@ -14,7 +14,6 @@ const HomeGallery =({w, h})=> {
     return (
       <div>
         <Parallax
-          blur={{ min: 15, max: -15 }}
           bgImage={'https://sikra.s3.us-east-2.amazonaws.com/necklaces/ammonite.jpg'}
           bgImageAlt="ammonite" 
           strength={-200}
@@ -23,7 +22,7 @@ const HomeGallery =({w, h})=> {
         </Parallax>
   
         <Parallax
-          blur={{ min: 15, max: -15 }} 
+          blur={{ min: 10, max: -10 }} 
           bgImage={'https://sikra.s3.us-east-2.amazonaws.com/necklaces/owl-group.jpg'}
           bgImageAlt="owls" 
           strength={-200}
@@ -31,7 +30,7 @@ const HomeGallery =({w, h})=> {
           <div style={{width:'370px', height: '400px'}} />
         </Parallax>
         <Parallax
-          blur={{ min: 15, max: -15 }} 
+          blur={{ min: 10, max: -10 }} 
           bgImage={'https://sikra.s3.us-east-2.amazonaws.com/rings/Dragon face moss.jpg'}
           bgImageAlt="dragon" 
           strength={-200}
@@ -39,7 +38,7 @@ const HomeGallery =({w, h})=> {
           <div style={{width:'370px', height: '400px'}} />
         </Parallax>
         <Parallax
-          blur={{ min: 15, max: -15 }}
+          blur={{ min: 10, max: -10 }}
           bgImage={'https://sikra.s3.us-east-2.amazonaws.com/rings/Fossil-Ring-Gold-Amber-Enamel-Outside-Side-cropped.jpg'}
           bgImageAlt="fossil ring"
           strength={-200}
