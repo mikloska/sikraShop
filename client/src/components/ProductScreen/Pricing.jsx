fd497cfa452adaaef250c545b47bfb2b0a54834a
mikloska <mikloska973@gmail.com>
2021-11-21 10:45:40 -0500

    Changed price appearance on product page.

--- client/src/components/ProductScreen/Pricing.jsx
+++ client/src/components/ProductScreen/Pricing.jsx
@@ -5,7 +5,7 @@ const Pricing = ({product, chain, chainObj, qty}) => {
   return(
     <div style={{marginTop:4}}>
       <Typography>Unit Price:   <strong>${product.category==='necklaces'? (product.price+chainObj[chain]) : product.price}</strong></Typography>
-      <Typography>Total Price:   <strong>${product.category==='necklaces'? ((product.price*qty) + (chainObj[chain]*qty)).toFixed(2) : ((product.price*qty).toFixed(2))}</strong></Typography>
+      <Typography>Total Price:   <strong>${product.category==='necklaces'? ((product.price*qty) + (chainObj[chain]*qty)) : ((product.price*qty))}</strong></Typography>
     </div>
   )
 }
