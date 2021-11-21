f8c25ae32cb506b540c845e7ff95ad2ce733f4c9
mikloska <mikloska973@gmail.com>
2021-11-21 00:03:45 -0500

    Changed Necklace type dropdown text styling.

--- client/src/components/ProductScreen/NecklaceOptions.jsx
+++ client/src/components/ProductScreen/NecklaceOptions.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import{Radio, RadioGroup, FormControl, FormLabel, FormControlLabel}  from '@material-ui/core/'
+import{Radio, RadioGroup, FormControl, FormLabel, FormControlLabel, Typography}  from '@material-ui/core/'
 
 const NecklaceOptions = ({formClass, chain, setChain}) => {
   return(
     <FormControl className={formClass} noValidate style={{border:'1px'}}>
-      <FormLabel component="legend">Necklace Material</FormLabel>
+      <FormLabel component="label">Necklace Material</FormLabel>
       <RadioGroup style={{margin:'auto'}} row onChange={(e)=>setChain(e.target.value)} value={chain}>
         <FormControlLabel control={<Radio  id='silver' value='silver' name="chain" />}label='silver'>Silver</FormControlLabel>
         <FormControlLabel control={<Radio  id='cord' value='cord' name="chain" />}label='cord'>Cord</FormControlLabel>
